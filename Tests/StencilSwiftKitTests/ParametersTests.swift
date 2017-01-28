7a93338c43ef7df2e33aaaed049834f668433909
David Jennes <david.jennes@appwise.be>
2017-01-28 16:56:37 +0100

    better key validation + more extensive tests

--- Tests/StencilSwiftKitTests/ParametersTests.swift
+++ Tests/StencilSwiftKitTests/ParametersTests.swift
@@ -22,9 +22,21 @@ class ParametersTests: XCTestCase {
     let result = try! Parameters.parse(items: items)
     
     XCTAssertEqual(result.count, 1, "1 parameter should have been parsed")
-    guard let sub = result["foo"] as? [String: String] else { XCTFail("Parsed parameter is a dictionary"); return }
+    guard let sub = result["foo"] as? [String: String] else { XCTFail("Parsed parameter should be a dictionary"); return }
     XCTAssertEqual(sub["baz"], "1")
     XCTAssertEqual(sub["bar"], "2")
+  }
+  
+  func testDeepStructured() {
+    let items = ["foo.bar.baz.qux=1"]
+    let result = try! Parameters.parse(items: items)
+    
+    XCTAssertEqual(result.count, 1, "1 parameter should have been parsed")
+    guard let foo = result["foo"] as? [String: Any] else { XCTFail("Parsed parameter should be a dictionary"); return }
+    guard let bar = foo["bar"] as? [String: Any] else { XCTFail("Parsed parameter should be a dictionary"); return }
+    guard let baz = bar["baz"] as? [String: Any] else { XCTFail("Parsed parameter should be a dictionary"); return }
+    guard let qux = baz["qux"] as? String else { XCTFail("Parsed parameter should be a string"); return }
+    XCTAssertEqual(qux, "1")
   }
   
   func testRepeated() {
@@ -32,10 +44,68 @@ class ParametersTests: XCTestCase {
     let result = try! Parameters.parse(items: items)
     
     XCTAssertEqual(result.count, 1, "1 parameter should have been parsed")
-    guard let sub = result["foo"] as? [String] else { XCTFail("Parsed parameter is an array"); return }
+    guard let sub = result["foo"] as? [String] else { XCTFail("Parsed parameter should be an array"); return }
     XCTAssertEqual(sub.count, 3, "Array has 3 elements")
     XCTAssertEqual(sub[0], "1")
     XCTAssertEqual(sub[1], "2")
     XCTAssertEqual(sub[2], "hello")
+  }
+  
+  func testInvalidSyntax() {
+    do {
+      let items = ["foo:1"]
+      _ = try Parameters.parse(items: items)
+    } catch ParametersError.invalidSyntax {
+      // That's the expected exception we want to happen
+    } catch let error {
+      XCTFail("Unexpected error occured while parsing: \(error)")
+    }
+    
+    do {
+      let items = ["foo!1"]
+      _ = try Parameters.parse(items: items)
+    } catch ParametersError.invalidSyntax {
+      // That's the expected exception we want to happen
+    } catch let error {
+      XCTFail("Unexpected error occured while parsing: \(error)")
+    }
+    
+    do {
+      let items = [""]
+      _ = try Parameters.parse(items: items)
+    } catch ParametersError.invalidSyntax {
+      // That's the expected exception we want to happen
+    } catch let error {
+      XCTFail("Unexpected error occured while parsing: \(error)")
+    }
+  }
+  
+  func testInvalidKey() {
+    do {
+      let items = ["foo:bar=1"]
+      _ = try Parameters.parse(items: items)
+    } catch ParametersError.invalidKey {
+      // That's the expected exception we want to happen
+    } catch let error {
+      XCTFail("Unexpected error occured while parsing: \(error)")
+    }
+    
+    do {
+      let items = [".=1"]
+      _ = try Parameters.parse(items: items)
+    } catch ParametersError.invalidKey {
+      // That's the expected exception we want to happen
+    } catch let error {
+      XCTFail("Unexpected error occured while parsing: \(error)")
+    }
+    
+    do {
+      let items = ["foo.=1"]
+      _ = try Parameters.parse(items: items)
+    } catch ParametersError.invalidKey {
+      // That's the expected exception we want to happen
+    } catch let error {
+      XCTFail("Unexpected error occured while parsing: \(error)")
+    }
   }
 }
