5dcf1218ab947c7f823416ef5f41f42ab2ffec8a
David Jennes <david.jennes@appwise.be>
2017-01-29 19:38:14 +0100

    rewrite tests

--- Tests/StencilSwiftKitTests/MapNodeTests.swift
+++ Tests/StencilSwiftKitTests/MapNodeTests.swift
@@ -5,27 +5,153 @@
 //
 
 import XCTest
+@testable import Stencil
 @testable import StencilSwiftKit
 
 class MapNodeTests: XCTestCase {
   static let context = [
-    "items1": ["one", "two", "three"],
-    "items2": ["hello", "world", "everyone"]
+    "items": ["one", "two", "three"]
   ]
   
-  func testBasic() {
-    let template = StencilSwiftTemplate(templateString: Fixtures.string(for: "map-basic.stencil"), environment: stencilSwiftEnvironment())
-    let result = try! template.render(MapNodeTests.context)
+  func testParser() {
+    let tokens: [Token] = [
+      .block(value: "map items into result"),
+      .text(value: "hello"),
+      .block(value: "endmap")
+    ]
     
-    let expected = Fixtures.string(for: "map-basic.out")
-    XCTDiffStrings(result, expected)
+    let parser = TokenParser(tokens: tokens, environment: stencilSwiftEnvironment())
+    guard let nodes = try? parser.parse(),
+      let node = nodes.first as? MapNode else {
+        XCTFail("Unable to parse tokens")
+        return
+    }
+
+    XCTAssertEqual(node.variable, Variable("items"))
+    XCTAssertEqual(node.resultName, "result")
+    XCTAssertNil(node.mapVariable)
+    XCTAssertEqual(node.nodes.count, 1)
+    XCTAssert(node.nodes.first is TextNode)
+  }
+    
+  func testParserWithMapVariable() {
+    let tokens: [Token] = [
+      .block(value: "map items into result using item"),
+      .text(value: "hello"),
+      .block(value: "endmap")
+    ]
+    
+    let parser = TokenParser(tokens: tokens, environment: stencilSwiftEnvironment())
+    guard let nodes = try? parser.parse(),
+      let node = nodes.first as? MapNode else {
+        XCTFail("Unable to parse tokens")
+        return
+    }
+    
+    XCTAssertEqual(node.variable, Variable("items"))
+    XCTAssertEqual(node.resultName, "result")
+    XCTAssertEqual(node.mapVariable, "item")
+    XCTAssertEqual(node.nodes.count, 1)
+    XCTAssert(node.nodes.first is TextNode)
   }
   
-  func testWithIndex() {
-    let template = StencilSwiftTemplate(templateString: Fixtures.string(for: "map-with-index.stencil"), environment: stencilSwiftEnvironment())
-    let result = try! template.render(MapNodeTests.context)
+  func testParserFail() {
+    // no closing tag
+    do {
+      let tokens: [Token] = [
+        .block(value: "map items into result"),
+        .text(value: "hello")
+      ]
+      
+      let parser = TokenParser(tokens: tokens, environment: stencilSwiftEnvironment())
+      XCTAssertThrowsError(try parser.parse())
+    }
+    
+    // no parameters
+    do {
+      let tokens: [Token] = [
+        .block(value: "map"),
+        .text(value: "hello"),
+        .block(value: "endmap")
+      ]
+      
+      let parser = TokenParser(tokens: tokens, environment: stencilSwiftEnvironment())
+      XCTAssertThrowsError(try parser.parse())
+    }
+    
+    // no result parameters
+    do {
+      let tokens: [Token] = [
+        .block(value: "map items"),
+        .text(value: "hello"),
+        .block(value: "endmap")
+      ]
+      
+      let parser = TokenParser(tokens: tokens, environment: stencilSwiftEnvironment())
+      XCTAssertThrowsError(try parser.parse())
+    }
+    
+    // no result variable name
+    do {
+      let tokens: [Token] = [
+        .block(value: "map items into"),
+        .text(value: "hello"),
+        .block(value: "endmap")
+      ]
+      
+      let parser = TokenParser(tokens: tokens, environment: stencilSwiftEnvironment())
+      XCTAssertThrowsError(try parser.parse())
+    }
+    
+    // no map variable name
+    do {
+      let tokens: [Token] = [
+        .block(value: "map items into result using"),
+        .text(value: "hello"),
+        .block(value: "endmap")
+      ]
+      
+      let parser = TokenParser(tokens: tokens, environment: stencilSwiftEnvironment())
+      XCTAssertThrowsError(try parser.parse())
+    }
+  }
 
-    let expected = Fixtures.string(for: "map-with-index.out")
-    XCTDiffStrings(result, expected)
+  func testRender() {
+    let context = Context(dictionary: MapNodeTests.context)
+    let node = MapNode(variable: "items", resultName: "result", mapVariable: nil, nodes: [TextNode(text: "hello")])
+    let output = try! node.render(context)
+    
+    XCTAssertEqual(output, "")
+  }
+  
+  func testContext() {
+    let context = Context(dictionary: MapNodeTests.context)
+    let node = MapNode(variable: "items", resultName: "result", mapVariable: "item", nodes: [TextNode(text: "hello")])
+    _ = try! node.render(context)
+    
+    guard let items = context["items"] as? [String], let result = context["result"] as? [String] else {
+      XCTFail("Unable to render map")
+      return
+    }
+    XCTAssertEqual(items, MapNodeTests.context["items"] ?? [])
+    XCTAssertEqual(result, ["hello", "hello", "hello"])
+  }
+  
+  func testMapLoopContext() {
+    let context = Context(dictionary: MapNodeTests.context)
+    let node = MapNode(variable: "items", resultName: "result", mapVariable: nil, nodes: [
+      VariableNode(variable: "maploop.counter"),
+      VariableNode(variable: "maploop.first"),
+      VariableNode(variable: "maploop.last"),
+      VariableNode(variable: "maploop.item")
+    ])
+    _ = try! node.render(context)
+    
+    guard let items = context["items"] as? [String], let result = context["result"] as? [String] else {
+      XCTFail("Unable to render map")
+      return
+    }
+    XCTAssertEqual(items, MapNodeTests.context["items"] ?? [])
+    XCTAssertEqual(result, ["0truefalseone", "1falsefalsetwo", "2falsetruethree"])
   }
 }
