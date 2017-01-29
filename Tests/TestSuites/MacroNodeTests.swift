01fbf145a16e23431823e22cd4497500093fa735
David Jennes <david.jennes@appwise.be>
2017-01-29 16:36:15 +0100

    test callable block

--- Tests/TestSuites/MacroNodeTests.swift
+++ Tests/TestSuites/MacroNodeTests.swift
@@ -50,13 +50,26 @@ class MacroNodeTests: XCTestCase {
   }
   
   func testParserFail() {
-    let tokens: [Token] = [
-      .block(value: "macro myFunc"),
-      .text(value: "hello")
-    ]
+    do {
+      let tokens: [Token] = [
+        .block(value: "macro myFunc"),
+        .text(value: "hello")
+      ]
+      
+      let parser = TokenParser(tokens: tokens, environment: stencilSwiftEnvironment())
+      XCTAssertThrowsError(try parser.parse())
+    }
     
-    let parser = TokenParser(tokens: tokens, environment: stencilSwiftEnvironment())
-    XCTAssertThrowsError(try parser.parse())
+    do {
+      let tokens: [Token] = [
+        .block(value: "macro"),
+        .text(value: "hello"),
+        .block(value: "endmacro")
+      ]
+      
+      let parser = TokenParser(tokens: tokens, environment: stencilSwiftEnvironment())
+      XCTAssertThrowsError(try parser.parse())
+    }
   }
   
   func testRender() {
@@ -73,5 +86,51 @@ class MacroNodeTests: XCTestCase {
     let output = try! node.render(context)
     
     XCTAssertEqual(output, "")
+  }
+  
+  func testContextModification() {
+    let node = MacroNode(variableName: "myFunc", parameters: [], nodes: [TextNode(text: "hello")])
+    let context = Context(dictionary: [:])
+    _ = try! node.render(context)
+    
+    guard let block = context["myFunc"] as? CallableBlock else {
+      XCTFail("Unable to render macro token")
+      return
+    }
+    XCTAssertEqual(block.parameters, [])
+    XCTAssertEqual(block.nodes.count, 1)
+    XCTAssert(block.nodes.first is TextNode)
+  }
+  
+  func testContextModificationWithParameters() {
+    let node = MacroNode(variableName: "myFunc", parameters: ["a", "b", "c"], nodes: [TextNode(text: "hello")])
+    let context = Context(dictionary: [:])
+    _ = try! node.render(context)
+    
+    guard let block = context["myFunc"] as? CallableBlock else {
+      XCTFail("Unable to render macro token")
+      return
+    }
+    XCTAssertEqual(block.parameters, ["a", "b", "c"])
+    XCTAssertEqual(block.nodes.count, 1)
+    XCTAssert(block.nodes.first is TextNode)
+  }
+  
+  func testCallableBlockRender() {
+    let block = CallableBlock(parameters: [], nodes: [TextNode(text: "hello")])
+    let context = Context(dictionary: [:])
+    
+    XCTAssertThrowsError(try block.render(context))
+  }
+  
+  func testCallableBlockContext() {
+    let block = CallableBlock(parameters: ["p1", "p2", "p3"], nodes: [TextNode(text: "hello")])
+    let arguments = [Variable("a"), Variable("b"), Variable("\"hello\"")]
+    let context = Context(dictionary: ["a": 1, "b": 2])
+    
+    let result = try! block.context(context, arguments: arguments)
+    XCTAssertEqual(result["p1"] as? Int, 1)
+    XCTAssertEqual(result["p2"] as? Int, 2)
+    XCTAssertEqual(result["p3"] as? String, "hello")
   }
 }
