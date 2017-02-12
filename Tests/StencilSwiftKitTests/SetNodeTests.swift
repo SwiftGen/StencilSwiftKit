55bfb3f229fe61b17964f7fc6e6972dee1666573
David Jennes <djbe@users.noreply.github.com>
2017-02-12 23:00:02 +0100

    Rewrite node tests (#17)

--- Tests/StencilSwiftKitTests/SetNodeTests.swift
+++ Tests/StencilSwiftKitTests/SetNodeTests.swift
@@ -5,26 +5,129 @@
 //
 
 import XCTest
+@testable import Stencil
 @testable import StencilSwiftKit
 
 class SetNodeTests: XCTestCase {
-  func testBasic() {
-    let template = StencilSwiftTemplate(templateString: Fixtures.string(for: "set-basic.stencil"), environment: stencilSwiftEnvironment())
-    let result = try! template.render([:])
+  func testParser() {
+    let tokens: [Token] = [
+      .block(value: "set value"),
+      .text(value: "true"),
+      .block(value: "endset")
+    ]
     
-    let expected = Fixtures.string(for: "set-basic.out")
-    XCTDiffStrings(result, expected)
+    let parser = TokenParser(tokens: tokens, environment: stencilSwiftEnvironment())
+    guard let nodes = try? parser.parse(),
+      let node = nodes.first as? SetNode else {
+      XCTFail("Unable to parse tokens")
+      return
+    }
+    
+    XCTAssertEqual(node.variableName, "value")
+    XCTAssertEqual(node.nodes.count, 1)
+    XCTAssert(node.nodes.first is TextNode)
   }
   
-  func testWithContext() {
-    let template = StencilSwiftTemplate(templateString: Fixtures.string(for: "set-with-context.stencil"), environment: stencilSwiftEnvironment())
-    let result = try! template.render([
-      "x": 1,
-      "y": 2,
-      "items": [1, 2, 3]
-    ])
+  func testParserFail() {
+    let tokens: [Token] = [
+      .block(value: "set value"),
+      .text(value: "true")
+    ]
     
-    let expected = Fixtures.string(for: "set-with-context.out")
-    XCTDiffStrings(result, expected)
+    let parser = TokenParser(tokens: tokens, environment: stencilSwiftEnvironment())
+    XCTAssertThrowsError(try parser.parse())
+  }
+  
+  func testRender() {
+    let node = SetNode(variableName: "value", nodes: [TextNode(text: "true")])
+    let context = Context(dictionary: [:])
+    let output = try! node.render(context)
+    
+    XCTAssertEqual(output, "")
+  }
+  
+  func testContextModification() {
+    // start empty
+    let context = Context(dictionary: [:])
+    XCTAssertNil(context["a"])
+    XCTAssertNil(context["b"])
+    
+    // set a
+    var node = SetNode(variableName: "a", nodes: [TextNode(text: "hello")])
+    _ = try! node.render(context)
+    XCTAssertEqual(context["a"] as? String, "hello")
+    XCTAssertNil(context["b"])
+    
+    // set b
+    node = SetNode(variableName: "b", nodes: [TextNode(text: "world")])
+    _ = try! node.render(context)
+    XCTAssertEqual(context["a"] as? String, "hello")
+    XCTAssertEqual(context["b"] as? String, "world")
+    
+    // modify a
+    node = SetNode(variableName: "a", nodes: [TextNode(text: "hi")])
+    _ = try! node.render(context)
+    XCTAssertEqual(context["a"] as? String, "hi")
+    XCTAssertEqual(context["b"] as? String, "world")
+  }
+  
+  func testWithExistingContext() {
+    // start with a=1, b=2
+    let context = Context(dictionary: ["a": 1, "b": 2])
+    XCTAssertEqual(context["a"] as? Int, 1)
+    XCTAssertEqual(context["b"] as? Int, 2)
+    
+    // set a
+    var node = SetNode(variableName: "a", nodes: [TextNode(text: "hello")])
+    _ = try! node.render(context)
+    XCTAssertEqual(context["a"] as? String, "hello")
+    XCTAssertEqual(context["b"] as? Int, 2)
+    
+    // set b
+    node = SetNode(variableName: "b", nodes: [TextNode(text: "world")])
+    _ = try! node.render(context)
+    XCTAssertEqual(context["a"] as? String, "hello")
+    XCTAssertEqual(context["b"] as? String, "world")
+  }
+  
+  func testContextPush() {
+    // start with a=1, b=2
+    let context = Context(dictionary: ["a": 1, "b": 2])
+    XCTAssertEqual(context["a"] as? Int, 1)
+    XCTAssertEqual(context["b"] as? Int, 2)
+    XCTAssertNil(context["c"])
+    
+    // set a
+    var node = SetNode(variableName: "a", nodes: [TextNode(text: "hello")])
+    _ = try! node.render(context)
+    XCTAssertEqual(context["a"] as? String, "hello")
+    XCTAssertEqual(context["b"] as? Int, 2)
+    XCTAssertNil(context["c"])
+    
+    // push context level
+    context.push() {
+      XCTAssertEqual(context["a"] as? String, "hello")
+      XCTAssertEqual(context["b"] as? Int, 2)
+      XCTAssertNil(context["c"])
+      
+      // set b
+      node = SetNode(variableName: "b", nodes: [TextNode(text: "world")])
+      _ = try! node.render(context)
+      XCTAssertEqual(context["a"] as? String, "hello")
+      XCTAssertEqual(context["b"] as? String, "world")
+      XCTAssertNil(context["c"])
+      
+      // set c
+      node = SetNode(variableName: "c", nodes: [TextNode(text: "foo")])
+      _ = try! node.render(context)
+      XCTAssertEqual(context["a"] as? String, "hello")
+      XCTAssertEqual(context["b"] as? String, "world")
+      XCTAssertEqual(context["c"] as? String, "foo")
+    }
+    
+    // after pop
+    XCTAssertEqual(context["a"] as? String, "hello")
+    XCTAssertEqual(context["b"] as? Int, 2)
+    XCTAssertNil(context["c"])
   }
 }
