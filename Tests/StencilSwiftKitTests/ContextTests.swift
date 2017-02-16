5f557d3ed943d0f6b35eb22e86fb9331e7264967
David Jennes <djbe@users.noreply.github.com>
2017-02-16 09:33:32 +0100

    Some small changes to enrich(context) function (#19)
    * small fix to match stencil context type
    * documentation + constants + env override

--- Tests/StencilSwiftKitTests/ContextTests.swift
+++ Tests/StencilSwiftKitTests/ContextTests.swift
@@ -13,38 +13,44 @@ class ContextTests: XCTestCase {
   func testEmpty() {
     let context = [String: Any]()
     
-    let result = try! enrich(context: context, parameters: [])
+    let result = try! StencilContext.enrich(context: context,
+                                            parameters: [],
+                                            environment: ["PATH": "foo:bar:baz"])
     XCTAssertEqual(result.count, 2, "2 items have been added")
     
-    guard let env = result["env"] as? [AnyHashable: Any] else { XCTFail("`env` should be a dictionary"); return }
-    XCTAssertNotNil(env["PATH"] as? String)
-    guard let params = result["param"] as? [AnyHashable: Any] else { XCTFail("`param` should be a dictionary"); return }
+    guard let env = result[StencilContext.environment] as? [String: Any] else { XCTFail("`env` should be a dictionary"); return }
+    XCTAssertEqual(env["PATH"] as? String, "foo:bar:baz")
+    guard let params = result[StencilContext.parameters] as? [String: Any] else { XCTFail("`param` should be a dictionary"); return }
     XCTAssertEqual(params.count, 0)
   }
   
   func testWithContext() {
     let context: [String : Any] = ["foo": "bar", "hello": true]
     
-    let result = try! enrich(context: context, parameters: [])
+    let result = try! StencilContext.enrich(context: context,
+                                            parameters: [],
+                                            environment: ["PATH": "foo:bar:baz"])
     XCTAssertEqual(result.count, 4, "4 items have been added")
     XCTAssertEqual(result["foo"] as? String, "bar")
     XCTAssertEqual(result["hello"] as? Bool, true)
     
-    guard let env = result["env"] as? [AnyHashable: Any] else { XCTFail("`env` should be a dictionary"); return }
-    XCTAssertNotNil(env["PATH"] as? String)
-    guard let params = result["param"] as? [AnyHashable: Any] else { XCTFail("`param` should be a dictionary"); return }
+    guard let env = result[StencilContext.environment] as? [String: Any] else { XCTFail("`env` should be a dictionary"); return }
+    XCTAssertEqual(env["PATH"] as? String, "foo:bar:baz")
+    guard let params = result[StencilContext.parameters] as? [String: Any] else { XCTFail("`param` should be a dictionary"); return }
     XCTAssertEqual(params.count, 0)
   }
   
   func testWithParameters() {
     let context = [String: Any]()
     
-    let result = try! enrich(context: context, parameters: ["foo=bar", "hello"])
+    let result = try! StencilContext.enrich(context: context,
+                                            parameters: ["foo=bar", "hello"],
+                                            environment: ["PATH": "foo:bar:baz"])
     XCTAssertEqual(result.count, 2, "2 items have been added")
     
-    guard let env = result["env"] as? [AnyHashable: Any] else { XCTFail("`env` should be a dictionary"); return }
-    XCTAssertNotNil(env["PATH"] as? String)
-    guard let params = result["param"] as? [AnyHashable: Any] else { XCTFail("`param` should be a dictionary"); return }
+    guard let env = result[StencilContext.environment] as? [String: Any] else { XCTFail("`env` should be a dictionary"); return }
+    XCTAssertEqual(env["PATH"] as? String, "foo:bar:baz")
+    guard let params = result[StencilContext.parameters] as? [String: Any] else { XCTFail("`param` should be a dictionary"); return }
     XCTAssertEqual(params["foo"] as? String, "bar")
     XCTAssertEqual(params["hello"] as? Bool, true)
   }
