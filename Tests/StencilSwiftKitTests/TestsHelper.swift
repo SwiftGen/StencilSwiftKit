a336166123b5e02f318607de9ef510ec27168fce
David Jennes <djbe@users.noreply.github.com>
2017-02-24 13:00:00 +0100

    Configure Circle CI (#20)

--- Tests/StencilSwiftKitTests/TestsHelper.swift
+++ Tests/StencilSwiftKitTests/TestsHelper.swift
@@ -8,10 +8,13 @@ import Foundation
 import XCTest
 import PathKit
 
-private let colorCode: (String) -> String = ProcessInfo().environment["XcodeColors"] == "YES" ? { "\u{001b}[\($0);" } : { _ in "" }
+private let colorCode: (String) -> String =
+  ProcessInfo().environment["XcodeColors"] == "YES" ? { "\u{001b}[\($0);" } : { _ in "" }
 private let (msgColor, reset) = (colorCode("fg250,0,0"), colorCode(""))
-private let okCode = (num: colorCode("fg127,127,127"), code: colorCode(""))
-private let koCode = (num: colorCode("fg127,127,127") + colorCode("bg127,0,0"), code: colorCode("fg250,250,250") + colorCode("bg127,0,0"))
+private let okCode = (num: colorCode("fg127,127,127"),
+                      code: colorCode(""))
+private let koCode = (num: colorCode("fg127,127,127") + colorCode("bg127,0,0"),
+                      code: colorCode("fg250,250,250") + colorCode("bg127,0,0"))
 
 func diff(_ result: String, _ expected: String) -> String? {
   guard result != expected else { return nil }
@@ -42,7 +45,14 @@ func diff(_ result: String, _ expected: String) -> String? {
     }
     let lhsNum = addLineNumbers(slice(lhsLines, 4)).joined(separator: "\n")
     let rhsNum = addLineNumbers(slice(rhsLines, 4)).joined(separator: "\n")
-    return "\(msgColor)Mismatch at line \(badLineIdx)\(reset)\n>>>>>> result\n\(lhsNum)\n======\n\(rhsNum)\n<<<<<< expected"
+    return [
+      "\(msgColor)Mismatch at line \(badLineIdx)\(reset)",
+      ">>>>>> result",
+      "\(lhsNum)",
+      "======",
+      "\(rhsNum)",
+      "<<<<<< expected"
+    ].joined(separator: "\n")
   }
   return nil
 }
