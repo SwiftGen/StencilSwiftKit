a63622b22d660c520ad9ab1639d9ece366c99dfe
Krzysztof Zabłocki <krzysztof.zablocki@pixle.pl>
2017-02-28 17:32:22 +0100

    Fix spm tags (#26)

--- Package.swift
+++ Package.swift
@@ -6,9 +6,7 @@ let package = Package(
         Target(name: "StencilSwiftKit", dependencies: [])
     ],
     dependencies: [
-        // https://github.com/kylef/Stencil/pull/84
-        .Package(url: "https://github.com/vknabel/Stencil.git", majorVersion: 0, minor: 7),
-        // Requires new release including https://github.com/kylef/PathKit/commit/7b17207
-        .Package(url: "https://github.com/vknabel/PathKit.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8),
     ]
 )
