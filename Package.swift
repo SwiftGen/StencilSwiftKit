5b3eb7898c8e99de91c1965997bcb95af7e10f83
Olivier Halligon <olivier@halligon.net>
2017-03-01 15:26:21 +0100

    Remove unnecessary PathKit dependency from SPM

--- Package.swift
+++ Package.swift
@@ -7,6 +7,5 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8),
     ]
 )
