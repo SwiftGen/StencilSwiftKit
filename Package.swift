1b9526fc79e30387df8cc053498b10b7054e4637
David Jennes <david.jennes@gmail.com>
2018-07-16 12:45:42 +0200

    Better conform to SPM recommended folder structure

--- Package.swift
+++ Package.swift
@@ -2,29 +2,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "StencilSwiftKit",
-    products: [
-        .library(name: "StencilSwiftKit", targets: ["StencilSwiftKit"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/stencilproject/Stencil.git", .upToNextMinor(from: "0.12.0"))
-    ],
-    targets: [
-      .target(
-        name: "StencilSwiftKit",
-        dependencies: [
-          "Stencil"
-        ],
-        path: "",
-        sources: ["Sources"]
-      ),
-      .testTarget(
-        name: "StencilSwiftKitTests",
-        dependencies: [
-          "StencilSwiftKit"
-        ],
-        path: "Tests"
-      )
-    ],
-    swiftLanguageVersions: [4]
+  name: "StencilSwiftKit",
+  products: [
+      .library(name: "StencilSwiftKit", targets: ["StencilSwiftKit"])
+  ],
+  dependencies: [
+      .package(url: "https://github.com/stencilproject/Stencil.git", .upToNextMinor(from: "0.12.0"))
+  ],
+  targets: [
+    .target(
+      name: "StencilSwiftKit",
+      dependencies: [
+        "Stencil"
+      ]
+    ),
+    .testTarget(
+      name: "StencilSwiftKitTests",
+      dependencies: [
+        "StencilSwiftKit"
+      ]
+    )
+  ],
+  swiftLanguageVersions: [4]
 )
