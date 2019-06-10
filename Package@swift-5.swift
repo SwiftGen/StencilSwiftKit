c53aec8ca4c4df7173d5ddd53b5eb52543e6ec48
Hector Matos <hectormatos2011@users.noreply.github.com>
2019-06-10 12:44:59 -0700

    Adds support for Swift 5 and removes warnings
    This adds support for Swift 5 as well as removing warnings when executing `swift build` on a project generated through `swift package generate-xcodeproj`

---
+++ Package@swift-5.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+  name: "StencilSwiftKit",
+  products: [
+      .library(name: "StencilSwiftKit", targets: ["StencilSwiftKit"])
+  ],
+  dependencies: [
+      .package(url: "https://github.com/stencilproject/Stencil.git", .upToNextMinor(from: "0.13.1"))
+  ],
+  targets: [
+    .target(
+      name: "StencilSwiftKit",
+      dependencies: [
+        "Stencil"
+      ]
+    ),
+    .testTarget(
+      name: "StencilSwiftKitTests",
+      dependencies: [
+        "StencilSwiftKit"
+      ]
+    )
+  ],
+  swiftLanguageVersions: [.v4_2, .v5.0]
+)
