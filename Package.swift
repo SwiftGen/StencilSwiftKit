2d2f323a5d017811a44048448fd869d04c471f03
David Jennes <david.jennes@gmail.com>
2020-10-14 20:24:01 +0200

    Add commit hooks (using Komondor)

--- Package.swift
+++ Package.swift
@@ -7,21 +7,32 @@ let package = Package(
     .library(name: "StencilSwiftKit", targets: ["StencilSwiftKit"])
   ],
   dependencies: [
+    .package(url: "https://github.com/shibapm/Komondor.git", from: "1.0.0"),
     .package(url: "https://github.com/stencilproject/Stencil.git", .upToNextMajor(from: "0.14.0"))
   ],
   targets: [
-    .target(
-      name: "StencilSwiftKit",
-      dependencies: [
-        "Stencil"
-      ]
-    ),
-    .testTarget(
-      name: "StencilSwiftKitTests",
-      dependencies: [
-        "StencilSwiftKit"
-      ]
-    )
+    .target(name: "StencilSwiftKit", dependencies: [
+      "Stencil"
+    ]),
+    .testTarget(name: "StencilSwiftKitTests", dependencies: [
+      "StencilSwiftKit"
+    ])
   ],
   swiftLanguageVersions: [.v5]
 )
+
+#if canImport(PackageConfig)
+import PackageConfig
+
+let config = PackageConfiguration([
+  "komondor": [
+    "pre-commit": [
+        "PATH=\"~/.rbenv/shims:$PATH\" bundler exec rake lint:code",
+        "PATH=\"~/.rbenv/shims:$PATH\" bundler exec rake lint:tests"
+    ],
+    "pre-push": [
+      "PATH=\"~/.rbenv/shims:$PATH\" bundle exec rake spm:test"
+    ]
+  ],
+]).write()
+#endif
